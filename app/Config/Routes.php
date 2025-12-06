bd8444e8c8a5120584661f5d7f47f66f330cce8c
Radiva Maulidin Pratama <radivamaulidin30@gmail.com>
2025-12-06 15:30:02 +0700

    Connect to database Keuangan_UMKM

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,15 +5,15 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-// index route
-$routes->get('/', 'Auth::login');
-// akses untuk staff
+$routes->get('/', 'Home::index');
 $routes->get('/staff/input_pembelian', 'Staff::input_pembelian');
 $routes->get('/staff/input_penjualan', 'Staff::input_penjualan');
+$routes->get('/test-db', function () {
+    try {
+        $db = \Config\Database::connect();
+        echo "Connected to: " . $db->getDatabase();
+    } catch (\Exception $e) {
+        echo $e->getMessage();
+    }
+});
 
-// akses untuk owner
-$routes->get('/owner/kelola_menu', 'Owner::kelola_menu');
-$routes->get('/owner/laporan_keuangan', 'Owner::laporan_keuangan');
-
-//akses untuk auth
-// $routes->get('/login', 'Auth::login');
